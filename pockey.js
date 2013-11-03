af1d0666f4407fd79c7b3249e77129e9a3e927b0
Thierry Nowak <thierry.nowak@pictime.com>
2013-11-03 19:26:25 +0100

    Create Angular module

--- pockey.js
+++ pockey.js
@@ -1,43 +1,44 @@
-function ListController($scope, $window) {
+angular.module('Pockey', [])
+	.controller('ListController', ['$scope', '$window', function ($scope, $window) {
 
-	var budget = 500;
+		var budget = 500;
 
-	$scope.categories = [
-	    { text : 'Repas',	icon : 'cutlery',	color : 'danger' },
-	    { text : 'Loisirs',	icon : 'film',		color : 'primary' },
-	    { text : 'Soirées',	icon : 'glass',		color : 'warning' },
-	    { text : 'Amis',	icon : 'user',		color : 'success' },
-	    { text : 'Divers',	icon : 'euro',		color : 'info' }
-	];
+		$scope.categories = [
+		    { text : 'Repas',	icon : 'cutlery',	color : 'danger' },
+		    { text : 'Loisirs',	icon : 'film',		color : 'primary' },
+		    { text : 'Soirées',	icon : 'glass',		color : 'warning' },
+		    { text : 'Amis',	icon : 'user',		color : 'success' },
+		    { text : 'Divers',	icon : 'euro',		color : 'info' }
+		];
 
-	$scope.expenses = [
-	    { date : '2013-10-15',	cost : 35,	category : $scope.categories[0] },
-	    { date : '2013-10-14',	cost : 12,	category : $scope.categories[1] },
-	    { date : '2013-10-10',	cost : 21,	category : $scope.categories[2] },
-	    { date : '2013-10-10',	cost : 5,	category : $scope.categories[3] },
-	    { date : '2013-10-08',	cost : 8,	category : $scope.categories[4] },
-	    { date : '2013-10-07',	cost : 15,	category : $scope.categories[2] }
-	];
+		$scope.expenses = [
+		    { date : '2013-10-15',	cost : 35,	category : $scope.categories[0] },
+		    { date : '2013-10-14',	cost : 12,	category : $scope.categories[1] },
+		    { date : '2013-10-10',	cost : 21,	category : $scope.categories[2] },
+		    { date : '2013-10-10',	cost : 5,	category : $scope.categories[3] },
+		    { date : '2013-10-08',	cost : 8,	category : $scope.categories[4] },
+		    { date : '2013-10-07',	cost : 15,	category : $scope.categories[2] }
+		];
 
-	$scope.findLastDate = function() {
-		var dates = new Array();
-		angular.forEach($scope.expenses, function(expense) {
-			dates.push(new Date(expense.date).getTime());
-		});
+		$scope.findLastDate = function() {
+			var dates = new Array();
+			angular.forEach($scope.expenses, function(expense) {
+				dates.push(new Date(expense.date).getTime());
+			});
+	
+			return max(dates);
+		};
 
-		return max(dates);
-	};
+		max = function(array) {
+			return $window.Math.max.apply(null, array);
+		};
 
-	max = function(array) {
-		return $window.Math.max.apply(null, array);
-	};
-
-	$scope.computeTotal = function() {
-		var allExpensesCost = 0;
-		angular.forEach($scope.expenses, function(expense) {
-			allExpensesCost += expense.cost;
-		});
-
-		return budget - allExpensesCost;
-	};
-}
+		$scope.computeTotal = function() {
+			var allExpensesCost = 0;
+			angular.forEach($scope.expenses, function(expense) {
+				allExpensesCost += expense.cost;
+			});
+	
+			return budget - allExpensesCost;
+		};
+	}]);
