a44fedebdae53e0a2ca9074a739a3b6d856bcd9d
Thierry Nowak <thierry.nowak@pictime.com>
2013-11-04 22:02:13 +0100

    Transform data into constant and values

--- pockey.js
+++ pockey.js
@@ -9,26 +9,32 @@ angular.module('Pockey', [])
 		};
 	})
 
-	.controller('ListController', ['$scope', '$window', function ($scope, $window) {
+	.constant('CATEGORIES', [
+	    { text : 'Repas',	icon : 'cutlery',	color : 'danger' },
+	    { text : 'Loisirs',	icon : 'film',		color : 'primary' },
+	    { text : 'Soirées',	icon : 'glass',		color : 'warning' },
+	    { text : 'Amis',	icon : 'user',		color : 'success' },
+	    { text : 'Divers',	icon : 'euro',		color : 'info' }
+	])
 
-		var budget = 500;
+	.factory('$data', function(CATEGORIES) {
+		return {
+			budget : 500,
 
-		$scope.categories = [
-		    { text : 'Repas',	icon : 'cutlery',	color : 'danger' },
-		    { text : 'Loisirs',	icon : 'film',		color : 'primary' },
-		    { text : 'Soirées',	icon : 'glass',		color : 'warning' },
-		    { text : 'Amis',	icon : 'user',		color : 'success' },
-		    { text : 'Divers',	icon : 'euro',		color : 'info' }
-		];
+			expenses : [
+			    { date : '2013-10-15',	cost : 35,	category : CATEGORIES[0] },
+			    { date : '2013-10-14',	cost : 12,	category : CATEGORIES[1] },
+			    { date : '2013-10-10',	cost : 21,	category : CATEGORIES[2] },
+			    { date : '2013-10-10',	cost : 5,	category : CATEGORIES[3] },
+			    { date : '2013-10-08',	cost : 8,	category : CATEGORIES[4] },
+			    { date : '2013-10-07',	cost : 15,	category : CATEGORIES[2] }
+			]
+		}
+	})
 
-		$scope.expenses = [
-		    { date : '2013-10-15',	cost : 35,	category : $scope.categories[0] },
-		    { date : '2013-10-14',	cost : 12,	category : $scope.categories[1] },
-		    { date : '2013-10-10',	cost : 21,	category : $scope.categories[2] },
-		    { date : '2013-10-10',	cost : 5,	category : $scope.categories[3] },
-		    { date : '2013-10-08',	cost : 8,	category : $scope.categories[4] },
-		    { date : '2013-10-07',	cost : 15,	category : $scope.categories[2] }
-		];
+	.controller('ListController', ['$scope', '$data', '$window', function ($scope, $data, $window) {
+
+		$scope.expenses = $data.expenses;
 
 		$scope.findLastDate = function() {
 			var dates = new Array();
@@ -49,6 +55,6 @@ angular.module('Pockey', [])
 				allExpensesCost += expense.cost;
 			});
 	
-			return budget - allExpensesCost;
+			return $data.budget - allExpensesCost;
 		};
 	}]);
