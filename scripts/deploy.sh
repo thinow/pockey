c3cd22f62afaadf2fc0de241ea2e6d93d3711320
Thierry Nowak <thinow@gmail.com>
2014-03-23 15:36:41 +0100

    Exit on error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+echo -n "Current folder is root     : "
+if [ -e firebase.json ]; then
+	echo "OK"
+else
+	echo "Error!"
+	exit 1
+fi
 
-echo -n "Check current folder     : " && [ -e firebase.json ] && echo "OK" || echo "Error!"
-echo -n "Check clean working copy : " && [ `git status | grep -c 'working directory clean'` -eq 1 ] && echo "OK" || echo "Error!"
+echo -n "Clean working copy         : "
+if [ `git status | grep -c 'working directory clean'` -eq 1 ]; then
+	echo "OK"
+else
+	echo "Error!"
+	exit 1
+fi
 
