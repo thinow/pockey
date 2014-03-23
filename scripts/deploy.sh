b4e531ce16fe46ec3b752a85ba8e71a5f35f06b4
Thierry Nowak <thinow@gmail.com>
2014-03-23 14:57:32 +0100

    Check clean working copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-echo -n "Check current folder : " && [ -e firebase.json ] && echo "OK" || echo "Error!"
+
+echo -n "Check current folder     : " && [ -e firebase.json ] && echo "OK" || echo "Error!"
+echo -n "Check clean working copy : " && [ `git status | grep -c 'working directory clean'` -eq 1 ] && echo "OK" || echo "Error!"
+
